d455b708181db1c588f334d9583d76b76bdd8733
jmgiusti91 <jmgiusti91@gmail.com>
2016-09-23 12:45:18 -0300

    Se agrego funcionalidad para abrir github del autor desde el navegaor del celular y no desde la misma app

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -374,14 +374,23 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('AutorCtrl', function($scope) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('AutorCtrl', function($scope, $ionicPlatform, $cordovaInAppBrowser) {
+
+  var options = {
+      location: 'yes',
+      clearcache: 'yes',
+      toolbar: 'no'
+    };
+
+  $scope.VerGitHub=function(){
+    $ionicPlatform.ready(function(){
+
+      $cordovaInAppBrowser.open('https://github.com/jmgiusti91', '_system', options);
+
+    });
+    
+  }
+  
 })
 
 .controller('LecturaCtrl', function($scope, $ionicPlatform, $cordovaFile){
